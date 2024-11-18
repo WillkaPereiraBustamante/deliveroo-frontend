74e6ddaffac92a858d12b1f3dc760bed3e431483
Willka <Willka@Host-004.lan>
2024-11-18 18:12:30 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -25,11 +25,43 @@ function App() {
     <span>En cours de chargement...</span>
   ) : (
     <div>
-      <span>{data.restaurant.name}</span>
+      {/* <Header/> */}
+      <div className="presentation">
+        <div className="presentation-title">
+          <h1>{data.restaurant.name}</h1>
+          <p>{data.restaurant.description}</p>
+        </div>
+        <div className="presentation-picture">
+          <img src={data.restaurant.picture} alt="restaurant table" />
+        </div>
+      </div>
+      
       <br />
-      <ul>
+      <ul className="content container">
         {data.categories.map((categories, index) => {
-          return <li key={index}>{categories.name}</li>;
+          return <li className="category" key={index}>
+            <h2>{categories.name}</h2>
+            <div className="carrousel">
+              {categories.meals.map((meals, id) => {
+                return <div className="product-card" key={id}>
+                  <div className="menu-item">
+                     <h3>{meals.title}</h3>
+                     <p>{meals.description}</p>
+                     <div className="menu-item-infos">
+                        <span>{meals.price} €</span>
+                        {meals.popular && 
+                          <span><i>*</i>Populaire</span>
+                        }
+                     </div>
+                  </div>
+                  {meals.picture && 
+                    <img src={meals.picture} alt="" />
+                  }
+                </div>
+              })}
+            </div>
+            
+            </li>;
         })}
       </ul>
     </div>
